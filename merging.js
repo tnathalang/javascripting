c83e26b87757f49d5faf8d930aacd47642eae77c
Tom Na <nathalang_t@hotmail.ca>
2018-12-19 23:38:15 +0000

    merging and sorting of arrays

---
+++ merging.js
@@ -0,0 +1,15 @@
+function merge(value1,value2) {
+ var merge = [];
+ merge = value1.concat(value2);
+
+ merge.sort(function(a,b) {
+  return a - b});
+
+ return merge
+ }
+
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
